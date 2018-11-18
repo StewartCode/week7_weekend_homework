5f2a7eadd308961908a0538e1c0fedc014c43f7b
StevieCode <sdcowan@hotmail.com>
2018-11-18 11:47:53 +0000

    1.3

--- src/views/map_add.js
+++ src/views/map_add.js
@@ -5,6 +5,7 @@ const Display_Results = require('./display_results.js');
 
 const MapAdd = function(target) {
   this.target = target;
+  //console.log(this.target);
 };
 
 let map;
@@ -30,38 +31,67 @@ MapAdd.prototype.addAPin = function(long, lat) {
   marker = new mapboxgl.Marker()
     .setLngLat([long, lat])
     .addTo(map);
-    //this.popup(long, lat);
-    //console.log(popupOffsets);
-  };
+  //this.popup(long, lat);
+  //console.log(popupOffsets);
+};
 
-  MapAdd.prototype.popup = function (long, lat, string) {
-    let markerHeight = 50,
-      markerRadius = 10,
-      linearOffset = 25;
-       popupOffsets = {
-      'top': [0, 0],
-      'top-left': [0, 0],
-      'top-right': [0, 0],
-      'bottom': [0, -markerHeight],
-      'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
-      'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
-      'left': [markerRadius, (markerHeight - markerRadius) * -1],
-      'right': [-markerRadius, (markerHeight - markerRadius) * -1]
-     };
-      popup = new mapboxgl.Popup({
+MapAdd.prototype.popup = function(long, lat, string) {
+
+//**************************************************************** to the next stars not working
+  map.on('mouseenter', 'places', function(e) {
+    console.log(e);
+    //Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
+
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var description = e.features[0].properties.description;
+    //    Ensure that if the map is zoomed out such that multiple
+    //    copies of the feature are visible, the popup appears
+    //    over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+
+      popup.setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+    }
+  });
+
+  map.on('mouseleave', 'places', function() {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
+
+//*******************************************************************
+
+  let markerHeight = 50,
+    markerRadius = 10,
+    linearOffset = 25;
+  popupOffsets = {
+    'top': [0, 0],
+    'top-left': [0, 0],
+    'top-right': [0, 0],
+    'bottom': [0, -markerHeight],
+    'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    'left': [markerRadius, (markerHeight - markerRadius) * -1],
+    'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+  };
+  popup = new mapboxgl.Popup({
       offset: popupOffsets,
       className: 'my-class'
     })
+
     .setLngLat([long, lat])
     .setHTML('<p id="description">Location :</p>' + string.name +
-            '<p id="description">Mass :</p>' + string.mass + " " + "tons" +
-            '<p id="description">Year :</p>' + string.year[0]+ string.year[1]+ string.year[2]+ string.year[3]
-  )
+      '<p id="description">Mass :</p>' + string.mass + " " + "Tons" +
+      '<p id="description">Year :</p>' + string.year[0] + string.year[1] + string.year[2] + string.year[3]
+    )
     .addTo(map);
 };
 
 
-MapAdd.prototype.addAPin2 = function(long, lat,) {
+MapAdd.prototype.addAPin2 = function(long, lat, ) {
   marker = new mapboxgl.Marker()
     .setLngLat([long, lat])
     .addTo(map);
